63f7ba968b54bffd0ce57189a140d84310eedc4b
Jamshed Qureshi <jamshed@Noxsix.local>
2020-04-06 09:54:28 -0400

    Add test

--- embark_demo/test/purchase_spec.js
+++ embark_demo/test/purchase_spec.js
@@ -1,50 +1,51 @@
 /*global artifacts, contract, config, it, assert, web3*/
-const Purchase = artifacts.require('Purchase');
+const Purchase = artifacts.require("Purchase");
+const BN = require("bn.js");
+const { Utils } = artifacts.require("EmbarkJS");
 
-let accounts;
-let buyerAddress;
-let sellerAddress;
 let price = 100000;
 let state = {
-  "CREATED" : 0,
-  "LOCKED" : 1,
-  "INACTIVE": 2
-}
+  CREATED: 0,
+  LOCKED: 1,
+  INACTIVE: 2
+};
 
-// For documentation please see https://framework.embarklabs.io/docs/contracts_testing.html
-config({
-  //blockchain: {
-  //  accounts: [
-  //    // you can configure custom accounts with a custom balance
-  //    // see https://framework.embarklabs.io/docs/contracts_testing.html#Configuring-accounts
-  //  ]
-  //},
-  contracts: {
-    deploy: {
-      "Purchase": {
-        args: [price],
-        fromIndex: 0
-      }
-    }
-  }
-}, (_err, web3_accounts) => {
-  accounts = web3_accounts;
-  buyerAddress = accounts[1];
-  sellerAddress = accounts[0];
-});
-
-contract("Purchase", function () {
+contract("Purchase", function(accounts) {
   this.timeout(0);
+  let PurchaseInstance;
+  let buyerAddress = accounts[1];
+  let sellerAddress = accounts[0];
 
-  it("Should deploy purchase", async function () {
-    let result = await Purchase.options.address;
+  before(async () => {
+    return new Promise(async (resolve, reject) => {
+      const gas = await Purchase.deploy({ arguments: [price] }).estimateGas();
+
+      Utils.secureSend(
+        web3,
+        Purchase.deploy({ arguments: [price] }),
+        { gas, from: sellerAddress },
+        true,
+        function(err, receipt) {
+          if (err) {
+            return reject(err);
+          }
+          PurchaseInstance = Purchase;
+          PurchaseInstance.options.address = receipt.contractAddress;
+          resolve();
+        }
+      );
+    });
+  });
+
+  it("Should deploy purchase", async function() {
+    let result = await PurchaseInstance.options.address;
     let contractState = await Purchase.state();
     assert.ok(contractState == state["CREATED"]);
     assert.ok(result.length > 0);
   });
 
-  it("Buyer deposits funds and confirms purchase", async function(){
-    let result = await Purchase.methods.confirmPurchase().send({
+  it("Buyer deposits funds and confirms purchase", async function() {
+    let result = await PurchaseInstance.methods.confirmPurchase().send({
       from: buyerAddress,
       value: price
     });
@@ -59,41 +60,73 @@ contract("Purchase", function () {
     assert.ok(contractState == state["LOCKED"]);
   });
 
-  it("Buyer confirm received", async function(){
+  it("Buyer confirm received", async function() {
     // test here
-    //confirmReceived
-    let result = await Purchase.method.confirmReceived().send();
+    let sellerBalance = await web3.eth.getBalance(sellerAddress);
+    sellerBalance = new BN(sellerBalance);
+    let result = await PurchaseInstance.methods.confirmReceived().send({
+      from: buyerAddress
+    });
+    let contractBuyerAddress = await PurchaseInstance.buyer();
+    let contractSellerAddress = await PurchaseInstance.seller();
+    let contractState = await PurchaseInstance.state();
 
-    let contractBuyerAddress = await Purchase.buyer();
-    let contractState = await Purchase.state();
-    let contractBalance = await web3.eth.getBalance(Purchase.options.address);
+    let contractBalance = await web3.eth.getBalance(
+      PurchaseInstance.options.address
+    );
+    let sellerBalanceNew = await web3.eth.getBalance(sellerAddress);
+    sellerBalanceNew = new BN(sellerBalanceNew);
 
+    const sellerChange = sellerBalanceNew.sub(sellerBalance);
     assert.ok(contractBuyerAddress == buyerAddress);
     assert.ok(contractSellerAddress == sellerAddress);
+    assert.ok(contractBalance == 0);
+    assert.ok(sellerChange.toNumber() == price);
     assert.ok(contractState == state["INACTIVE"]);
+  });
 
-  })
+  contract("Purchase and abort", () => {
+    before(async () => {
+      return new Promise(async (resolve, reject) => {
+        const gas = await Purchase.deploy({ arguments: [price] }).estimateGas();
 
-  it("Seller aborts item", async function(){
-    // test here
-    let result = await Purchase.method.abort().send();
+        Utils.secureSend(
+          web3,
+          Purchase.deploy({ arguments: [price] }),
+          { gas, from: sellerAddress },
+          true,
+          function(err, receipt) {
+            if (err) {
+              return reject(err);
+            }
+            PurchaseInstance = Purchase;
+            PurchaseInstance.options.address = receipt.contractAddress;
+            resolve();
+          }
+        );
+      });
+    });
+    it("Should deploy purchase", async function() {
+      let result = await PurchaseInstance.options.address;
+      let contractState = await Purchase.state();
+      assert.ok(contractState == state["CREATED"]);
+      assert.ok(result.length > 0);
+    });
 
-    let contractState = await Purchase.state();
-    let contractBalance = await web3.eth.getBalance(Purchase.options.address);
+    it("Seller aborts item", async function() {
+      // test here
+      let result = await PurchaseInstance.methods.abort().send({
+        from: sellerAddress
+      });
+      let contractSellerAddress = await PurchaseInstance.seller();
+      let contractState = await PurchaseInstance.state();
 
-    assert.ok(contractBalance == price);
-    assert.ok(contractState == state["INACTIVE"]);
-
-  })
-
-  // it("set storage value", async function () {
-  //   await SimpleStorage.methods.set(150).send({from: web3.eth.defaultAccount});
-  //   let result = await SimpleStorage.methods.get().call();
-  //   assert.strictEqual(parseInt(result, 10), 150);
-  // });
-
-  // it("should have account with balance", async function() {
-  //   let balance = await web3.eth.getBalance(accounts[0]);
-  //   assert.ok(parseInt(balance, 10) > 0);
-  // });
+      let contractBalance = await web3.eth.getBalance(
+        PurchaseInstance.options.address
+      );
+      assert.ok(contractSellerAddress == sellerAddress);
+      assert.ok(contractBalance == 0);
+      assert.ok(contractState == state["INACTIVE"]);
+    });
+  });
 });
